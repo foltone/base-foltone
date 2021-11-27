ae97f856244b47d88619e031bada9a8e119ad1da
foltone <77021937+foltone@users.noreply.github.com>
2021-11-27 21:31:49 +0100

    Update foltone.sql

--- foltone.sql
+++ foltone.sql
@@ -1,4 +1,20 @@
 --
+-- Structure de la table `society_moneywash`
+--
+
+CREATE TABLE `society_moneywash` (
+  
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(60) NOT NULL,
+  `society` varchar(60) NOT NULL,
+  `amount` int(11) NOT NULL,
+  
+  PRIMARY KEY (`id`)
+);
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `accounts`
 --
 
